ae721e0659690c9042fda99a97c1956b4ea56166
Mario Markwart <mario.markwart@de.abb.com>
2026-04-10 16:11:17 +0200

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Hero } from './hero/hero';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Hero],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected readonly title = signal('portfolio');
+}
